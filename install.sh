7f6794b12d3457780b62727a510f4156cf7c8722
Edvinas Bureika <edvinasbureika@gmail.com>
2026-04-05 12:09:00 +0300

    move configs to proper place

--- install.sh
+++ install.sh
@@ -2,5 +2,10 @@
 
 sudo dnf copr enable solopasha/hyprland -y
 
-sudo dnf install hyprland alacritty ranger grim slurp thunar rofi
+sudo dnf install hyprland alacritty ranger grim slurp thunar rofi -y
 
+mkdir -p ~/.config/hypr
+cp -r ./hypr/* ~/.config/hypr/
+
+mkdir -p ~/.config/waybar
+cp -r ./waybar/* ~/.config/waybar/
